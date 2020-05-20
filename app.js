e8bed61f4a9a3f0b78f1c4e08eb47326b2c156e7
Ian Muge <imuge94@gmail.com>
2020-05-20 13:16:01 +0300

    multi processing

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 'use strict';
 const express = require('express');
-const config =require('./config')
+const config = require('./config')
+module.exports = () => {
+  // Constants
+  const PORT = config.port;
+  const HOST = config.host;
 
-// Constants
-const PORT = config.port;
-const HOST = config.host;
+  // App
+  const app = express();
+  app.get('/', (req, res) => {
+    res.send('Hello World ');
+  });
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World ');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+  app.listen(PORT, HOST);
+  console.log(`Running on http://${HOST}:${PORT}`);
+};
